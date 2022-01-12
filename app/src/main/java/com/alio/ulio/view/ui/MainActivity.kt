62550635dc52086669530abe8cced6516142d574
jinseokha <seok270@gmail.com>
2022-01-12 15:33:35 +0900

    .

---
+++ app/src/main/java/com/alio/ulio/view/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alio.ulio.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alio.ulio.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
